1d83f44ea7f7d67cf6c4e2c1bb513201352fc706
qiuqiuball <ecust_yxwang@qq.com>
2025-07-28 22:20:25 +0800

    B站教程P6 给人物添加刚体组件，碰撞体组件，控制脚本; 添加inputSystem库(1.5.1版本) 添加一个红色的长方形碰撞体组件，让人物在受重力作用下可站立在其上

---
+++ Project/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    public Vector2 inputDirection;
+
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+}
